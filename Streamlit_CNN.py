256eac440a4363df444668fd37a8f8a904c17e1b
Kodipaka Chathurya Goud <122029749+ChathuryaKodipaka@users.noreply.github.com>
2024-09-19 21:48:47 -0400

    Update Streamlit_CNN.py

--- Streamlit_CNN.py
+++ Streamlit_CNN.py
@@ -3,11 +3,20 @@ import tensorflow as tf
 import cv2
 import numpy as np
 from PIL import Image
+import gdown
+
+# Define the Google Drive URL for your model
+file_id = '1-Xoepf6oNhh3QwGi0EnrRXiqlZxblcg8'
+download_url = f"https://drive.google.com/uc?id={file_id}"
+
+# Download the model file from Google Drive
+model_path = 'cnn_model.keras'
+gdown.download(download_url, model_path, quiet=False)
 
 # Load the trained model
-model = tf.keras.models.load_model('/Users/chathurya/Desktop/cnn_model.keras')
+model = tf.keras.models.load_model(model_path)
 
-# Define class labels (reverse the dictionary)
+# Define class labels
 class_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 
 # Streamlit app title
